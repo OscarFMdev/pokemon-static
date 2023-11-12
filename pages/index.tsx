be9c194b91471525e8a580f680300cc61030a5d3
Oscar Fernández Muñoz <oscarfmdev@gmail.com>
2023-11-12 17:38:43 -0600

    Add margin top

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ export default function HomePage({ pokemons }: Props) {
     <>
       <Layout title='Pokemon List'>
         <>
-          <ul className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4'>
+          <ul className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 mt-5'>
             {pokemons.map(({id, name, img}) => (
               <Card 
                 key={id} 
